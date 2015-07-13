f8a13bde96828f65886779ebf9e38b4c8673a676
modm <modongming91@gmail.com>
2015-07-13 13:56:35 +0800

    add mybatis example

---
+++ src/main/java/com/github/dormymo/ssmframework/dao/ITableDAO.java
@@ -0,0 +1,11 @@
+package com.github.dormymo.ssmframework.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by modm on 2015/7/13.
+ */
+@Repository
+public interface ITableDAO {
+    int count();
+}
