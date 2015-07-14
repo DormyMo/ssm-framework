0a006553d77b9835d9b125a69f24e3882a7ecac1
modm <modongming91@gmail.com>
2015-07-14 14:22:30 +0800

    add mybatis cache example

--- src/main/java/com/github/dormymo/ssmframework/controller/IndexController.java
+++ src/main/java/com/github/dormymo/ssmframework/controller/IndexController.java
@@ -2,11 +2,12 @@ package com.github.dormymo.ssmframework.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping(value = "hello")
+	@RequestMapping(value = "hello",method = RequestMethod.GET)
 	public String helloWorld(){
 		return "index";
 	}
